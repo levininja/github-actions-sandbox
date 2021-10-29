d787ec8a9800389b56a180fa353dc0a6054edfbb
Levi Hobbs <levi.hobbs@daveramsey.com>
2021-10-29 13:57:55 -0500

    Edited readme and the main.yml so it does readme mods

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-export const taxationIs = () => {
-    return "good?";
-}
+// use node's filesystem API to modify files
+const fs = require('fs');
+
+// modify the readme's counter and commit it back up to a branch
+const contents = fs.readFileSync(`${__dirname}/README.md`, 'utf8');
+const currentNumber = Number(contents);
+const newNumber = currentNumber + 1;
+fs.writeFileSync("README.md", newNumber.toString());
